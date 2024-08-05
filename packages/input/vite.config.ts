75096513de7306390758a1fd47f5ec8003923344
pinchao <cpc4253@163.com>
2024-08-05 16:48:05 +0800

    chore: 取消mjs在包文件中添加typemodule

--- packages/input/vite.config.ts
+++ packages/input/vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+// import { defineConfig } from 'vite';
+// import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      entry: './src/index.ts',
-      name: 'PeiliuiInput',
-      fileName: 'peili-ui-input',
-    },
-    minify: false,
-    rollupOptions: {
-      external: [/@peili-ui.*/, 'vue'],
-    },
-  },
-});
+// export default defineConfig({
+//   plugins: [vue()],
+//   build: {
+//     lib: {
+//       entry: './src/index.ts',
+//       name: 'PeiliuiInput',
+//       fileName: 'peili-ui-input',
+//     },
+//     minify: false,
+//     rollupOptions: {
+//       external: [/@peili-ui.*/, 'vue'],
+//     },
+//   },
+// });
+
+import { generateVueConfig } from '../build/scripts';
+
+export default generateVueConfig();
